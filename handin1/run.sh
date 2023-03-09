4933ea2e5768f5cb7d04b7699cd829b76a62e655
Rens Kievit <kievit@student36.strw.leidenuniv.nl>
2023-03-09 11:30:22 +0100

    Final Shell Update

--- handin1/run.sh
+++ handin1/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'Starting Hand-In 1'
 
-if [! -d 'results']; then
+if [ ! -d 'results' ]; then
   echo "Directory results does not exist. Creating it.."
   mkdir results
 fi
@@ -13,11 +13,11 @@ wget https://home.strw.leidenuniv.nl/~daalen/Handin_files/Vandermonde.txt
 
 echo 'Running Q1: Poisson Distribution..'
 python3 poisson.py
-
+	
 echo 'Running Q2: Vandermonde Matrix..'
 python3 vandermonde.py
 
 echo 'Start making LaTeX PDF..'
 
 pdflatex latex/main.tex
-
+pdflatex latex/main.tex # Do again to get references right
